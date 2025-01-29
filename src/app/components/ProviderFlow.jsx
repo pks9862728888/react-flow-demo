1f519fcff7deac7fdbc8e24371762d1abb939ff8
pks9862728888 <pks9862728888@gmail.com>
2025-01-29 16:11:14 +0530

    formatted file to have 2 space tab

--- src/app/components/ProviderFlow.jsx
+++ src/app/components/ProviderFlow.jsx
@@ -1,35 +1,19 @@
 "use client";
-import React, { useCallback } from 'react';
-import {
-  Background,
-  ReactFlow,
-  ReactFlowProvider,
-  addEdge,
-  useNodesState,
-  useEdgesState,
-} from '@xyflow/react';
+import React, {useCallback} from 'react';
+import {addEdge, Background, ReactFlow, ReactFlowProvider, useEdgesState, useNodesState,} from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
 
 const initialNodes = [
-  {
-    id: '1',
-    type: 'input',
-    data: { label: 'Node 1' },
-    position: { x: 250, y: 5 },
-  },
-  { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 100 } },
-  { id: '3', data: { label: 'Node 3' }, position: { x: 400, y: 100 } },
-  { id: '4', data: { label: 'Node 4' }, position: { x: 400, y: 200 } },
+  {id: '1', type: 'input', data: {label: 'Node 1'}, position: {x: 250, y: 5}},
+  {id: '2', data: {label: 'Node 2'}, position: {x: 100, y: 100}},
+  {id: '3', data: {label: 'Node 3'}, position: {x: 400, y: 100}},
+  {id: '4', data: {label: 'Node 4'}, position: {x: 400, y: 200}},
 ];
 
 const initialEdges = [
-  {
-    id: 'e1-2',
-    source: '1',
-    target: '2',
-  },
-  { id: 'e1-3', source: '1', target: '3' },
+  {id: 'e1-2', source: '1', target: '2'},
+  {id: 'e1-3', source: '1', target: '3'},
 ];
 
 function ProviderFlow() {
@@ -49,9 +33,9 @@ function ProviderFlow() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         fitView
-        style={{ backgroundColor: "#F7F9FB" }}
+        style={{backgroundColor: "#F7F9FB"}}
       >
-        <Background />
+        <Background/>
       </ReactFlow>
     </ReactFlowProvider>
   );
