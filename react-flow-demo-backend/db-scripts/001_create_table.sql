8b55aec248d1143ef10800eaa13d00d248563f3e
pks9862728888 <pks9862728888@gmail.com>
2025-02-02 23:10:55 +0530

    Added sql script to create and rollback tables

---
+++ react-flow-demo-backend/db-scripts/001_create_table.sql
@@ -0,0 +1,51 @@
+CREATE TABLE core.asset
+(
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL,
+    version INT          NOT NULL,
+    latest  BOOLEAN      NOT NULL,
+    created TIMESTAMP    NOT NULL,
+    updated TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE core.asset_data
+(
+    id         SERIAL PRIMARY KEY,
+    asset_id   BIGINT REFERENCES asset (id),
+    field_name VARCHAR(255) NOT NULL,
+    key        VARCHAR(255) NOT NULL,
+    created    TIMESTAMP    NOT NULL,
+    updated    TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE core.asset_data_transformation
+(
+    id            SERIAL PRIMARY KEY,
+    asset_id      BIGINT REFERENCES asset (id),
+    asset_data_id BIGINT REFERENCES asset_data (id),
+    rule_id       VARCHAR(255) NOT NULL,
+    description   TEXT         NOT NULL,
+    type          VARCHAR(255) NOT NULL,
+    created       TIMESTAMP    NOT NULL,
+    updated       TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE core.asset_edge
+(
+    id        SERIAL PRIMARY KEY,
+    source    BIGINT REFERENCES asset (id) NOT NULL,
+    target    BIGINT REFERENCES asset (id) NOT NULL,
+    created   TIMESTAMP NOT NULL,
+    updated   TIMESTAMP NOT NULL
+);
+
+CREATE TABLE core.asset_data_edge
+(
+    id        SERIAL PRIMARY KEY,
+    source    BIGINT REFERENCES asset (id) NOT NULL,
+    target    BIGINT REFERENCES asset (id) NOT NULL,
+    source_id BIGINT REFERENCES asset_data (id) NOT NULL,
+    target_id BIGINT REFERENCES asset_data (id) NOT NULL,
+    created   TIMESTAMP NOT NULL,
+    updated   TIMESTAMP NOT NULL
+);
---
+++ react-flow-demo-backend/db-scripts/001_create_table_rollback.sql
@@ -0,0 +1,5 @@
+DROP TABLE core.asset_data_edge;
+DROP TABLE core.asset_edge;
+DROP TABLE core.asset_data_transformation;
+DROP TABLE core.asset_data;
+DROP TABLE core.asset;
