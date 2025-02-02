22ddc2e8ed938d1178f5180e4c742894ab493ea2
pks9862728888 <pks9862728888@gmail.com>
2025-02-02 10:50:33 +0530

    Refactored methods to small files

--- src/app/components/DataLineageProviderFlow/DataLineageProviderFlow.tsx
+++ src/app/components/DataLineageProviderFlow/DataLineageProviderFlow.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {RefObject, useCallback, useEffect, useRef, useState} from 'react';
+import React, {RefObject, useCallback, useEffect, useRef} from 'react';
 import {
   addEdge,
   Background,
@@ -17,48 +17,13 @@ import {nodeTypes} from "@/app/constants/nodeTypes";
 import {LineageDataType} from "@/app/types/LineageDataType";
 import {EdgeBase} from "@xyflow/system";
 import {initialEdges, initialNodes} from "@/app/constants/data";
-import {SelectedNodeType} from "@/app/types/SelectedNodeType";
 import {ReactFlowNode} from "@/app/types/ReactFlowNode";
 import {cloneDeep} from "lodash";
-import updateSelectionDataInTableNode from "@/app/functions/tablenode/updateSelectionDataInTableNode";
 import buildTableNodeGraph from "@/app/functions/tablenode/buildTableNodeGraph";
-import getEdgeHandleKeyPrefix from "@/app/functions/tablenode/getEdgeHandleKeyPrefix";
 import buildGraphLineageDataMap from "@/app/functions/tablenode/buildGraphLineageDataMap";
-import updateEdgeSelectionConnectedToTableDataRows
-  from "@/app/functions/tablenode/updateEdgeSelectionConnectedToTableDataRows";
+import onTableNodeRowSelection from "@/app/components/DataLineageProviderFlow/onTableNodeRowSelection";
 
 const nodeClassName = (node: any) => node.type;
-
-function doOnTableNodeRowSelection(
-  setNodes: (value: (((prevState: ReactFlowNode[]) => ReactFlowNode[]) | ReactFlowNode[])) => void,
-  lineageDataMap: Map<string, Set<string>>,
-  setEdges: (value: (((prevState: Edge[]) => Edge[]))) => void) {
-  const onNodeSelect: (selectedNode: SelectedNodeType) => void = (newSelectedNode: SelectedNodeType): void => {
-    const selNdDataId: string = newSelectedNode.nodeId;
-    const selNdDataRowId: string = newSelectedNode.dataRowId;
-    const selectedFieldKey: string = getEdgeHandleKeyPrefix(selNdDataId, selNdDataRowId);
-
-    // Update the node lineage data state
-    setNodes((oldNodes: ReactFlowNode[]): ReactFlowNode[] => {
-        const adjacentNodes: Set<string> = lineageDataMap.get(selectedFieldKey) ?? new Set<string>();
-        return oldNodes.map((oldNode: ReactFlowNode): ReactFlowNode => {
-          let newNode: ReactFlowNode = cloneDeep(oldNode);
-          updateSelectionDataInTableNode(newNode, adjacentNodes, selectedFieldKey);
-          return newNode;
-        });
-      }
-    );
-
-    // Update the edge selection behavior
-    setEdges((oldEdges: Edge[]): Edge[] => {
-      const lineageData: Set<string> = lineageDataMap.get(selectedFieldKey) ?? new Set<string>();
-      return oldEdges.map((oldEdge: Edge): Edge => {
-        return updateEdgeSelectionConnectedToTableDataRows(oldEdge, lineageData);
-      });
-    });
-  }
-  return onNodeSelect;
-}
 
 const DataLineageProviderFlow: React.FC = () => {
   // React hooks to store state
@@ -91,7 +56,7 @@ const DataLineageProviderFlow: React.FC = () => {
 
   // Bind onNodeSelect event to lineageData for child components to trigger onNodeSelect action
   useEffect((): void => {
-    const onNodeSelect = doOnTableNodeRowSelection(
+    const onNodeSelect = onTableNodeRowSelection(
       setNodes, lineageDataRef.current, setEdges);
     nodes.forEach((node: ReactFlowNode): void => {
       node.data.triggerNodeSelection = onNodeSelect;
